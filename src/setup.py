2c852b63907dc7a57452c6ed5857a025301182ca
monotonee <monotonee@tuta.io>
2017-02-21 17:45:55 -0500

    Corrected package name in setup.py.

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 setup(
-    name='django-forced-fields',
+    name='django-forcedfields',
     version='0.0.1',
 
     author='monotonee',
