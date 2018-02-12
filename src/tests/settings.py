3f92f4e85f26f68807e5b6c8e634b608cf3bf1b2
monotonee <monotonee@tuta.io>
2018-02-12 15:02:26 -0500

    Clarified comments for silenced Django system checks.

--- src/tests/settings.py
+++ src/tests/settings.py
@@ -5,10 +5,10 @@ See:
     https://docs.djangoproject.com/en/dev/topics/testing/advanced/#using-the-django-test-runner-to-test-reusable-applications
 
 Originally, I attempted to leave the "default" DATABASES key empty and to define each database by a
-non-default, explicit config label. However, despite my use of database routers, the Django TestCase
-still produced an error when tearing down the test case when the "default" config was empty. The
-Django bug report below describes the error. For now, I'm just going to set "default" to point to
-the MySQL instance as I'm tired of fighting with Django over anything remotely unusual.
+non-default, explicit config label. However, despite my use of database routers, the Django
+TestCase still produced an error when tearing down the test case when the "default" config was
+empty. The Django bug report below describes the error. For now, I'm just going to set "default" to
+point to the MySQL instance as I'm tired of fighting with Django over anything remotely unusual.
 
 See:
     https://code.djangoproject.com/ticket/25504
@@ -52,12 +52,13 @@ INSTALLED_APPS = [
 
 # Silence checks system warning:
 #
-# "HINT: It seems you set a fixed date / time / datetime value as default for this field."
-# This may not be what you want. If you want to have the current date as default, use
+# "HINT: It seems you set a fixed date / time / datetime value as default for this field." This may
+# not be what you want. If you want to have the current date as default, use
 # `django.utils.timezone.now`.
 #
 # Django seems to want one to pass a callable as the value of the field's "default" option. It needs
-# to shut up. I know exactly what I'm doing and what I want.
+# to shut up. I know exactly what I'm doing and what I want. I need a fixed date in order to
+# complete certain tests in the test suite.
 #
 # See:
 #     https://github.com/django/django/blob/master/django/db/models/fields/__init__.py
